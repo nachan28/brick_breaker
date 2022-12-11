d2c892bfa3ceaaaf46329e365222549e64fafec7
nachan28 <torajiro282828@gmail.com>
2022-12-12 00:06:18 +0900

    update: updated draw() to make ball move

--- script.js
+++ script.js
@@ -19,12 +19,20 @@ const ctx = canvas.getContext("2d");
 // ctx.stroke();
 // ctx.closePath();
 
-function draw(){
-    ctx.beginPath();
-    ctx.arc(50, 50, 10, 0, Math.PI * 2, false);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+const dx = 2;
+const dy = -2;
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2, false);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+  x += dx;
+  y += dy;
 }
 
 setInterval(draw, 10);
