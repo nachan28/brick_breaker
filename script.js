b8df9e3bc4792d0828f508d22f2fd155f17b90bb
nachan28 <torajiro282828@gmail.com>
2022-12-12 18:17:34 +0900

    add: erase a brick by changing status property when collision detected

--- script.js
+++ script.js
@@ -35,15 +35,17 @@ for (let c = 0; c < brickColumnCount; c++) {
 function drawBricks() {
   for (let c = 0; c < brickColumnCount; c++) {
     for (let r = 0; r < brickRowCount; r++) {
-      const brickX = c * (brickWidth + brickPadding) + brickOfsetLeft;
-      const brickY = r * (brickHeight + brickPadding) + brickOfsetTop;
-      bricks[c][r].x = brickX;
-      bricks[c][r].y = brickY;
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
+      if (bricks[c][r].status === 1) {
+        const brickX = c * (brickWidth + brickPadding) + brickOfsetLeft;
+        const brickY = r * (brickHeight + brickPadding) + brickOfsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
     }
   }
 }
@@ -94,6 +96,7 @@ function collisionDetection() {
         y <= b.y + brickHeight
       ) {
         dy = -dy;
+        b.status = 0;
       }
     }
   }
