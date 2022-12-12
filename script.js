d0628d6dec9e43ab4a0eb832cd7c6a2c0335d8e0
nachan28 <torajiro282828@gmail.com>
2022-12-12 11:39:04 +0900

    fix: change dx, dy from constants to variables to make ball bounce

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ const ctx = canvas.getContext("2d");
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-const dx = 2;
-const dy = -2;
+let dx = 2;
+let dy = -2;
 const ballRadius = 10;
 
 function drawBall() {
