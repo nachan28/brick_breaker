0e012d4720861b71de5c4f3d6abbf3c0e97fb257
nachan28 <torajiro282828@gmail.com>
2022-12-14 17:48:23 +0900

    fix: fix typo

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ const brickColumnCount = 5;
 const brickWidth = 75;
 const brickHeight = 20;
 const brickPadding = 10;
-const brickOfsetTop = 30;
-const brickOfsetLeft = 30;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
 
 const bricks = [];
 let score = 0;
@@ -39,8 +39,8 @@ function drawBricks() {
   for (let c = 0; c < brickColumnCount; c++) {
     for (let r = 0; r < brickRowCount; r++) {
       if (bricks[c][r].status === 1) {
-        const brickX = c * (brickWidth + brickPadding) + brickOfsetLeft;
-        const brickY = r * (brickHeight + brickPadding) + brickOfsetTop;
+        const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
         bricks[c][r].x = brickX;
         bricks[c][r].y = brickY;
         ctx.beginPath();
